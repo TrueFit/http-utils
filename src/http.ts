8f3bce683e24bd2d16a13b92c0ae1a4616c3924c
Josh Gretz <jgretz@truefit.io>
2020-10-08 15:12:03 -0400

    update dependencies

--- src/http.ts
+++ src/http.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import axios, {AxiosInstance, AxiosPromise} from 'axios';
 import {sharedConfig} from './configure';
